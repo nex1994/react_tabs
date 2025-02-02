cac3e2cbe0e8265c9feaf0206656e0c5232044eb
nex1994 <bilicki1994@gmail.com>
2025-02-02 14:41:21 +0100

    init commit

--- src/components/Tabs/Tabs.tsx
+++ src/components/Tabs/Tabs.tsx
@@ -1,1 +1,38 @@
-export const Tabs = () => {};
+type Props = {
+  tabs: {
+    id: string;
+    title: string;
+    content: string;
+  }[];
+  isSelected: string;
+  onTabSelected: (tabID: string) => void;
+};
+
+export const Tabs = ({ tabs, isSelected, onTabSelected }: Props) => {
+  return (
+    <div data-cy="TabsComponent">
+      <div className="tabs is-boxed">
+        <ul>
+          {tabs.map(tab => {
+            return (
+              <li
+                key={tab.id}
+                className={isSelected === tab.id ? 'is-active' : ''}
+                data-cy="Tab"
+                onClick={() => onTabSelected(tab.id)}
+              >
+                <a href={`#${tab.id}`} data-cy="TabLink">
+                  {tab.title}
+                </a>
+              </li>
+            );
+          })}
+        </ul>
+      </div>
+
+      <div className="block" data-cy="TabContent">
+        {tabs.map(tab => (tab.id === isSelected ? tab.content : ''))}
+      </div>
+    </div>
+  );
+};
